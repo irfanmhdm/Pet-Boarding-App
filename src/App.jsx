c40343f5217a5cdb2eea70430bee32e6b3bdc23b
Muhammed Irfan S <irfanmhdm123@gmail.com>
2026-07-15 02:51:52 +0530

    View pet completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Add from './components/Add'
+import View from './components/View'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +15,7 @@ function App() {
 
     <BrowserRouter>
     <Routes>
-
+      <Route path ="/" element ={<View/>}/>
       <Route path ="/" element ={<Add/>}/>
     </Routes>
     </BrowserRouter>
